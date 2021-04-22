c857cf249090fd94243d1745717bbc53c02b3d87
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-04-22 16:48:09 -0600

    Style basic homepage option cards

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import './HomePage.css'
 import { ReactComponent as LeaderShip } from "../../Images/LeaderShip.svg";
+import { ReactComponent as Micropohone } from "../../Images/Microphone.svg";
 import Header from '../Header/Header.js'
 import { Link } from 'react-router-dom'
 
@@ -17,10 +18,11 @@ function HomePage() {
             <h2 className='leadership-title' data-cy='leadership-title'>Leadership</h2>
           </Link>
           <Link to='inspirational' className='catagory inspirational' data-cy='catagory-inspirational'>
-            <h2 className='inspirational-title' data-cy='inspirational-title'>Inspirational</h2>
+            <h2 className='inspirational-title' data-cy='inspirational-title'>Inspirational☆ ☆ ☆ ☆ </h2>
           </Link>
           <Link to='laughs' className='catagory laughs' data-cy='catagory-laughs'>
-            <h2 className='laughs-title' data-cy='laughs-title'>Laughs</h2>
+            <h2 className='laughs-title' data-cy='laughs-title'>Laughs<Micropohone className='microphone'/></h2>
+            
           </Link>
         </section>
         <button>Favorites</button> 
