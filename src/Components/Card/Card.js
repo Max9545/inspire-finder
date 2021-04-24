3266e10dfe3cd1b148f99d6c89eefeccac6432db
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-04-23 19:32:46 -0600

    Style toggle button

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -5,7 +5,7 @@ function Card({ quote, author, toggleFavorite, id, quoteType }) {
     <div className={`card ${quoteType}-card`} data-cy='card'>
       <p className={`quote ${quoteType}-quote`} data-cy='quote'>{quote}</p>
       <p className={`author ${quoteType}-author`} data-cy='author'>-- {author}</p>
-      <button className='toggle-favorite' data-cy='toggle-favorite'onClick={() => toggleFavorite({ id: id, quote: quote, author: author,  quoteType: quoteType})}>Toggle Whether To Remember This One</button>
+      <button className='toggle-favorite' data-cy='toggle-favorite'onClick={() => toggleFavorite({ id: id, quote: quote, author: author,  quoteType: quoteType})}>( + ) Toggle Remembrance ( - )</button>
     </div>
   )
 }
