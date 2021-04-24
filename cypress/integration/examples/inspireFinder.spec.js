4cf9e11e6250f29cfbaa623e45dc59a68246979f
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-04-24 17:20:49 -0600

    Finish Homepage test and start Hope page test

--- cypress/integration/examples/inspireFinder.spec.js
+++ cypress/integration/examples/inspireFinder.spec.js
@@ -1,34 +1,32 @@
-/// <reference types="cypress" />
-
-context('Actions', () => {
+context('Inspire Finder', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
 
-  // https://on.cypress.io/interacting-with-elements
-
-  it.only('Should have a Header', () => {
-    // https://on.cypress.io/type
-    cy.get('[data-cy=home-page').should('exist')
-    .get('[data-cy=header-container').should('exist')
-    .get('[data-cy=header-title').should('exist')
+  it('Should have a Header', () => {
+    cy.get('[data-cy=home-page]').should('exist')
+    .get('[data-cy=header-container]').should('exist')
+    .get('[data-cy=header-title]').should('exist')
     // .should('contain', 'Inspire Finder')
   })
 
-  it('Should have a Home Page', () => {
-    // https://on.cypress.io/type
-    cy.get('[data-cy=header-container').should('exist')
-    .get('[data-cy=select-catagory').should('exist')
-    .get('[data-cy=catagory-hope').should('exist')
-      
+  it.only('Should have a Home Page', () => {
+    cy.get('[data-cy=header-container]').should('exist')
+    .get('[data-cy=select-catagory]').should('exist')
+    .get('[data-cy=catagory-hope]').should('exist')
+    .get('[data-cy=catagory-leadership]').should('exist')
+    .get('[data-cy=catagory-inspirational]').should('exist')
+    .get('[data-cy=catagory-laughs]').should('exist')
   })
 
   it('Should have a Hope Quotes Page', () => {
-    // https://on.cypress.io/type
-    cy.get('[data-cy=header-container').should('exist')
-    .get('[data-cy=select-catagory').should('exist')
-    .get('[data-cy=catagory-hope').should('exist')
-      
+    cy.get('[data-cy=select-catagory]').should('exist')
+    .get('[data-cy=hope-title]').should('exist')
+    .should('contain', 'Hope')
+    .click()
+    .get('[data-cy=card-display-header]').should('exist')
+    .should('contain', 'hope Quotes')
+    
   })
 
 
--- src/Components/CardDisplay/CardDisplay.js
+++ src/Components/CardDisplay/CardDisplay.js
@@ -35,6 +35,7 @@ function CardDisplay({ quoteType, toggleFavorite}) {
             </>
           )
         }
+
       })
       setCurrentCards(latestCards)
     }
