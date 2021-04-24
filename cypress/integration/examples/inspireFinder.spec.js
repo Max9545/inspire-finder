ef500725b8cdc55c283b9ce8b66b93c1e128120f
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-04-24 17:40:42 -0600

    Finsih Hope page test

--- cypress/integration/examples/inspireFinder.spec.js
+++ cypress/integration/examples/inspireFinder.spec.js
@@ -1,6 +1,8 @@
 context('Inspire Finder', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy
+    // .intercept('https://favqs.com/api/quotes/?filter=hope&type=tag', { fixtures: 'hopeQuotes.json'})
+    .visit('http://localhost:3000')
   })
 
   it('Should have a Header', () => {
@@ -10,7 +12,7 @@ context('Inspire Finder', () => {
     // .should('contain', 'Inspire Finder')
   })
 
-  it.only('Should have a Home Page', () => {
+  it('Should have a Home Page', () => {
     cy.get('[data-cy=header-container]').should('exist')
     .get('[data-cy=select-catagory]').should('exist')
     .get('[data-cy=catagory-hope]').should('exist')
@@ -19,14 +21,21 @@ context('Inspire Finder', () => {
     .get('[data-cy=catagory-laughs]').should('exist')
   })
 
-  it('Should have a Hope Quotes Page', () => {
+  it.only('Should have a Hope Quotes Page that displays quotes', () => {
     cy.get('[data-cy=select-catagory]').should('exist')
     .get('[data-cy=hope-title]').should('exist')
     .should('contain', 'Hope')
     .click()
-    .get('[data-cy=card-display-header]').should('exist')
+    .get('[data-cy=card-display-header]')
+    .should('exist')
     .should('contain', 'hope Quotes')
-    
+    .get('[data-cy=card]').should('exist')
+    .get('[data-cy=quote]').should('exist')
+    .should('contain', 'Because tomorrow, the sun will rise. Who knows what the tide may bring.')
+    .get('[data-cy=author]').should('exist')
+    .should('contain', 'Chuck Noland')
+    .get('[data-cy=toggle-favorite]').should('exist')
+    .should('contain', '( + ) Toggle Remembrance ( - )')
   })
 
 
